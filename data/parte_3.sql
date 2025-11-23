2a2f9146bac96cdc186e456c76412952a311d0fd
Joao <jcarapinabarbosa@gmail.com>
2025-11-23 16:36:30 -0300

    Idade base clientes

--- data/parte_3.sql
+++ data/parte_3.sql
@@ -3,4 +3,4 @@ SELECT  IdCliente,
         datetime(substr(DtCriacao,1,19)) AS DtCriacao,
         julianday('now') - julianday(substr(DtCriacao,1,10)) AS IdadeBase
 FROM clientes
-ORDER BY 3 DESC
+ORDER BY IdadeBase DESC 
