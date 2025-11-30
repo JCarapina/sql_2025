f009c97f335ce1f13e1a41565abf6e368b6458db
Joao <jcarapinabarbosa@gmail.com>
2025-11-30 20:38:53 -0300

    Adicionando CTEs no projeto

--- data/projeto.sql
+++ data/projeto.sql
@@ -1,4 +1,3 @@
--- Quantidade de transações históricas(vida, D7, D14, D28, D56)
 WITH tb_transacoes AS (
     SELECT  Idcliente,
             Idtransacao,
@@ -6,15 +5,47 @@ WITH tb_transacoes AS (
             datetime(substr(DtCriacao,1,19)) AS DtCriacao,
             julianday('now') - julianday(substr(DtCriacao,1,10)) AS diffDate
     FROM transacoes
+),
+
+tb_cliente AS (
+    SELECT  IdCliente,
+            datetime(substr(DtCriacao,1,19)) AS DtCriacao,
+            julianday('now') - julianday(substr(DtCriacao,1,10)) AS IdadeBase
+
+    FROM clientes
+), 
+
+tb_sumario_transacoes AS (
+
+    SELECT  IdCliente,
+            count(Idtransacao) AS QtdeTransacoesVida,
+            count(CASE WHEN diffDate <= 56 THEN 1 END) AS QtdeTransacoesD56,
+            count(CASE WHEN diffDate <= 28 THEN 1 END) AS QtdeTransacoesD28,
+            count(CASE WHEN diffDate <= 14 THEN 1 END) AS QtdeTransacoesD14,
+            count(CASE WHEN diffDate <= 7 THEN 1 END) AS QtdeTransacoesD7
+
+            sum(qtdePontos) AS saldoPontos,
+
+            min(diffDate) AS diasUltimaInteracao,
+
+            sum(CASE WHEN qtdePontos > 0 THEN qtdePontos ELSE 0 END) AS PositAcumuladosVida,
+            sum(CASE WHEN qtdePontos > 0 AND diffDate < 56 THEN qtdePontos ELSE 0 END) AS PositAcumuladosD56,
+            sum(CASE WHEN qtdePontos > 0 AND diffDate < 28 THEN qtdePontos ELSE 0 END) AS PositAcumuladosD28,
+            sum(CASE WHEN qtdePontos > 0 AND diffDate < 14 THEN qtdePontos ELSE 0 END) AS PositAcumuladosD14,
+            sum(CASE WHEN qtdePontos > 0 AND diffDate <  7 THEN qtdePontos ELSE 0 END) AS PositAcumuladosD7,
+
+            sum(CASE WHEN qtdePontos > 0 THEN qtdePontos ELSE 0 END) AS NegativoAcumuladosVida,
+            sum(CASE WHEN qtdePontos > 0 AND diffDate < 56 THEN qtdePontos ELSE 0 END) AS NegativoAcumuladosD56,
+            sum(CASE WHEN qtdePontos > 0 AND diffDate < 28 THEN qtdePontos ELSE 0 END) AS NegativoAcumuladosD28,
+            sum(CASE WHEN qtdePontos > 0 AND diffDate < 14 THEN qtdePontos ELSE 0 END) AS NegativoAcumuladosD14,
+            sum(CASE WHEN qtdePontos > 0 AND diffDate <  7 THEN qtdePontos ELSE 0 END) AS NegativoAcumuladosD7
+
+    FROM tb_transacoes
+    GROUP BY IdCliente
 )
 
-SELECT  IdCliente,
-        count(Idtransacao) AS QtdeTransacoesVida,
-        count(CASE WHEN diffDate <= 56 THEN 1 END) AS QtdeTransacoesD56,
-        count(CASE WHEN diffDate <= 28 THEN 1 END) AS QtdeTransacoesD28,
-        count(CASE WHEN diffDate <= 14 THEN 1 END) AS QtdeTransacoesD14,
-        count(CASE WHEN diffDate <= 7 THEN 1 END) AS QtdeTransacoesD7
+SELECT  t1.*,
+        t2.IdadeBase
 
-
-FROM tb_transacoes
-GROUP BY IdCliente
+FROM tb_sumario_transacoes
+LEFT JOIN
