f22188b4cf5ccab8297b89af634d1659ac0ce7f1
Joao <jcarapinabarbosa@gmail.com>
2025-11-30 22:13:58 -0300

    juntado CTEs da parte_8

--- data/projeto.sql
+++ data/projeto.sql
@@ -42,12 +42,83 @@ tb_sumario_transacoes AS (
 
     FROM tb_transacoes
     GROUP BY IdCliente
+),
+
+tb_transacao_produto AS (
+SELECT  t1.*,
+        t2.idProduto,
+        t3.DescNomeProduto,
+        t3.DescCategoriaProduto
+
+FROM tb_transacoes AS t1
+
+LEFT JOIN transacao_produto AS t2
+ON t1.IdTransacao = t2.IdTransacao
+
+LEFT JOIN produtos AS t3
+ON t2.IdProduto = t3.IdProduto
+),
+tb_cliente_produto AS (
+    SELECT  IdCliente,
+            DescNomeProduto,
+            count(*) AS qtdeVida,
+            count(CASE WHEN diffDate <= 56 THEN IdTransacao END) AS qtde56,
+            count(CASE WHEN diffDate <= 28 THEN IdTransacao END) AS qtde28,
+            count(CASE WHEN diffDate <= 14 THEN IdTransacao END) AS qtde14,
+            count(CASE WHEN diffDate <= 7 THEN IdTransacao END) AS qtde7
+
+
+    FROM tb_transacao_produto
+
+    GROUP BY IdCliente, DescNomeProduto
+),
+
+tb_cliente_produto_rn AS ( 
+    SELECT *,
+        row_number() OVER (PARTITION BY IdCliente ORDER BY qtdeVida DESC) AS rnVida,
+        row_number() OVER (PARTITION BY IdCliente ORDER BY qtde56 DESC) AS rn56,
+        row_number() OVER (PARTITION BY IdCliente ORDER BY qtde28 DESC) AS rn28,
+        row_number() OVER (PARTITION BY IdCliente ORDER BY qtde14 DESC) AS rn14,
+        row_number() OVER (PARTITION BY IdCliente ORDER BY qtde7 DESC) AS rn7
+    FROM tb_cliente_produto
+),
+
+
+tb_join AS  (
+
+    SELECT  t1.*,
+            t2.IdadeBase,
+            t3.DescNomeProduto AS produtoVida,
+            t4.DescNomeProduto AS produto56,
+            t5.DescNomeProduto AS produto28,
+            t6.DescNomeProduto AS produto14,
+            t7.DescNomeProduto AS produto7
+
+    FROM tb_sumario_transacoes AS t1
+
+    LEFT JOIN tb_cliente AS t2
+    ON t1.IdCliente = t2.IdCliente
+
+    LEFT JOIN tb_cliente_produto_rn AS t3
+    ON t1.idCliente = t3.IdCliente
+    AND t3.rnVida = 1
+
+    LEFT JOIN tb_cliente_produto_rn AS t4
+    ON t1.idCliente = t4.IdCliente
+    AND t4.rn56 = 1
+
+    LEFT JOIN tb_cliente_produto_rn AS t5
+    ON t1.idCliente = t5.IdCliente
+    AND t5.rn28 = 1
+    
+    LEFT JOIN tb_cliente_produto_rn AS t6
+    ON t1.idCliente = t6.IdCliente
+    AND t6.rn14 = 1
+
+    LEFT JOIN tb_cliente_produto_rn AS t7
+    ON t1.idCliente = t7.IdCliente
+    AND t7.rn7 = 1
 )
 
-SELECT  t1.*,
-        t2.IdadeBase
-
-FROM tb_sumario_transacoes AS t1
-
-LEFT JOIN tb_cliente AS t2
-ON t1.IdCliente = t2.IdCliente
+SELECT * FROM tb_join
+ORDER BY idCliente
