249b3d6e9a43ba3e75e9e04adaf1b3e53a9ef6c6
Joao <jcarapinabarbosa@gmail.com>
2025-12-02 07:04:49 -0300

    Periodo mais ativo por cliente

---
+++ data/parte_9.sql
@@ -0,0 +1,35 @@
+-- Periodo mais ativo
+WITH tb_transacoes AS (
+    SELECT  Idcliente,
+            Idtransacao,
+            QtdePontos,
+            datetime(substr(DtCriacao,1,19)) AS DtCriacao,
+            julianday('now') - julianday(substr(DtCriacao,1,10)) AS diffDate,
+            CAST(strftime('%H', substr(DtCriacao,1,19)) AS INTEGER) AS DtHora
+    FROM transacoes 
+),
+
+tb_cliente_periodo AS (
+    SELECT  Idcliente,
+            CASE 
+                WHEN DtHora BETWEEN 7 AND 12 THEN 'MANHÃ'
+                WHEN DtHora BETWEEN 13 AND 18 THEN 'TARDE'
+                WHEN DtHora BETWEEN 19 AND 23 THEN 'NOITE'
+                WHEN DtHora BETWEEN 24 AND 6 THEN 'MADRUGADA'
+                ELSE 'SEM INFORMAÇÃO'
+            END AS Periodo,
+            COUNT(*) AS qtdeTransacao
+    FROM tb_transacoes
+
+    GROUP BY 1,2
+),
+
+tb_cliente_periodo_rn AS (
+SELECT *,
+        ROW_NUMBER() OVER (PARTITION BY IdCliente ORDER BY qtdeTransacao DESC) AS rnPeriodo
+
+FROM tb_cliente_periodo
+)
+
+SELECT  * FROM tb_cliente_periodo_rn
+WHERE rnPeriodo =1 
