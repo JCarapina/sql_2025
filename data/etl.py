f956bda71acdf27e1abcbc83ff6d7aff7e09b313
Joao <jcarapinabarbosa@gmail.com>
2025-12-10 05:51:45 -0300

    update sql

--- data/etl.py
+++ data/etl.py
@@ -2,5 +2,15 @@
 
 with open("etl_projeto.sql") as open_file:
     query = open_file.read()
+
 print(query)
+# %%
+
+dates = [
+    '2025-01-01',
+    '2025-01-02',
+    '2025-01-03',
+    '2025-01-04',
+    '2025-01-05',
+]
 # %%
